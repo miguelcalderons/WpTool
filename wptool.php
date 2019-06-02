e5a654da0ca48c49b2bcb0d311bc44c74877524b
Miguel Calderon <miguelcalderon10@gmail.com>
2019-06-02 17:10:46 +0200

    initial commit

---
+++ wptool.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name:       WP Tools
+ * Description:       A plugin to add register form with shortcode and edit user.
+ * Version:           1.0.0
+ * License:           GPL-2.0+
+ * Author:            Miguel Calderón
+ * Text Domain:       wptools
+ */
+
+require_once plugin_dir_path( __FILE__ ) . '/includes/register.php';
