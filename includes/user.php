126f9a8490adb4d75487ff3f604b177eba80fa8e
Miguel Calderon <miguelcalderon10@gmail.com>
2019-06-29 14:24:08 +0200

    add constants

---
+++ includes/user.php
@@ -0,0 +1,110 @@
+<?php 
+
+class User {
+
+    public const  LANGUAGES = [
+        "Afrikanns",
+        "Albanian",
+        "Arabic",
+        "Armenian",
+        "Azerbaijanian",
+        "Basque",
+        "Bengali",
+        "Bulgarian",
+        "Catalan",
+        "Cambodian",
+        "Chinese (Mandarin)",
+        "Croation",
+        "Czech",
+        "Danish",
+        "Dutch",
+        "English",
+        "Estonian",
+        "Fiji",
+        "Finnish",
+        "French",
+        "Georgian",
+        "German",
+        "Greek",
+        "Gujarati",
+        "Hebrew",
+        "Hindi",
+        "Hungarian",
+        "Icelandic",
+        "Indonesian",
+        "Irish",
+        "Italian",
+        "Japanese",
+        "Javanese",
+        "Korean",
+        "Latin",
+        "Latvian",
+        "Lithuanian",
+        "Macedonian",
+        "Malay",
+        "Malayalam",
+        "Maltese",
+        "Maori",
+        "Marathi",
+        "Mongolian",
+        "Nepali",
+        "Norwegian",
+        "Persian",
+        "Polish",
+        "Portuguese",
+        "Punjabi",
+        "Quechua",
+        "Romanian",
+        "Russian",
+        "Samoan",
+        "Serbian",
+        "Slovak",
+        "Slovenian",
+        "Spanish",
+        "Swahili",
+        "Swedish ",
+        "Tamil",
+        "Tatar",
+        "Telugu",
+        "Thai",
+        "Tibetan",
+        "Tonga",
+        "Turkish",
+        "Ukranian",
+        "Urdu",
+        "Uzbek",
+        "Vietnamese",
+        "Welsh",
+        "Xhosa"
+    ];
+
+    public const  YEARS = ['Currently studying', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011'
+        , '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003', '2002', '2001', '2000'
+        , '1999', '1998', '1997', '1996', '1995', '1994', '1993', '1992', '1991', '1990'
+        , '1989', '1988', '1987', '1986', '1985', '1984', '1983', '1982', '1981', '1980'];
+
+    public const  GENDERS = ['na', 'male', 'female'];
+
+    public const  EDU_LEVELS = ['High School', 'Associate´s Degree', 'Bachelor´s Degree', 'Master´s Degree', 'Doctor of Medicine (M.d.)', 'Doctor of Philosophy (Ph. D.)'];
+
+    public const USER_STATUSES = [
+        'Inactive',
+        'Incomplete',
+        'Complete',
+        'Complete + CV',
+    ];
+
+    public const  FOW = [
+        "Accounting & Finance",             
+        "Banking & Finance Services",       
+        "Education",                        
+        "Engineering",                      
+        "Healthcare",                       
+        "Human Resources",                  
+        "IT",                               
+        "Legal",                            
+        "Procurement",                      
+        "Sales & Marketing",                
+    ];
+
+}
---
+++ tests/test-user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class User
+ *
+ * @package Wptool
+ */
+
+/**
+ * User test case.
+ */
+class UserTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function testConstants() {
+        // Replace this with some actual testing code.
+        $this->assertIsArray(User::LANGUAGES);
+    }
+}
