ad4e434ca1225a1263d7bc3b28f9f790f11e78e2
Miguel <admin2@Mac-mini.local>
2026-02-03 12:02:36 -0400

    fix all error

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,25 @@
-var gulp        = require('gulp');
-var sass        = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
 
-function style() {
-  return gulp
-    .src(paths.styles.src)
-    .pipe(sass())
-    .on("error", sass.logError)
-    .pipe(gulp.dest(paths.styles.dest));
-}
-
-exports.style = style;
-
-
-function watch() {
-  style();
-
-  gulp.watch(paths.styles.src, style);
-}
-
-exports.watch = watch
-
-
-var paths = {
+const paths = {
   styles: {
     src: "sass/**/*.scss",
     dest: "styles"
   }
-
 };
+
+function style() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function watch() {
+  style();
+  gulp.watch(paths.styles.src, style);
+}
+
+exports.style = style;
+exports.watch = watch;
+exports.default = style;
